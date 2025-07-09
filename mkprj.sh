09e03d62d6724cde7ed71f2abaf3fc5ebbdc0546
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-07-09 19:18:23 +0200

    Aded MIT license and author to script

--- mkprj.sh
+++ mkprj.sh
@@ -2,23 +2,27 @@
 
 collect_prams(){
   OPTIND=1
-  while getopts "n:t:l:" opt; do
+  while getopts "a:n:t:l:" opt; do
     case $opt in
+      a) author="$OPTARG" ;;
       n) name="$OPTARG" ;;
       t) template="$OPTARG" ;;
       l) license="$OPTARG" ;;
       \?)
-        echo "Usage: $0 -n <name> -t <template> -l <license>"
+        echo "Usage: $0 -a <author> -n <name> -t <template> -l <license>"
         exit 1
         ;;
       :)
         echo "Option -$OPTARG requires an argument."
-        echo "Usage: $0 -n <name> -t <template> -l <license>"
+        echo "Usage: $0 -a <author> -n <name> -t <template> -l <license>"
         exit 1
         ;;
     esac
   done
 
+  if [ -z "$author" ]; then
+    read -p "Enter author name (-a): " author
+  fi
   if [ -z "$name" ]; then
     read -p "Enter project name (-n): " name
   fi
@@ -32,17 +36,30 @@ collect_prams(){
 
 log_prams(){
   echo "Building project using:"
+  echo "  Aurhor: $author"
   echo "  Name: $name"
   echo "  Template: $template"
   echo "  License: $license"
 }
 
 copy_repo_template() {
+mkdir .temp
+git clone git@github.com:EzraKirn/mkprj.git .temp
+cp -r .temp/$template/* .
+cp -r .temp/$template/.* .
+cp -r .temp/lisenses/$license ./LICENSE
+rm -rf .temp
+rm ./**/.gitkeep
 }
 
 main(){
   collect_prams "$@"
   log_prams
+
+  mkdir $name
+  cd $name
+
+  copy_repo_template
 }
 
 main "$@"
