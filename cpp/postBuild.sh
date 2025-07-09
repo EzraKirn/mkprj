ead4ad8f2861ff6005c0a022cbf2504d3bbb9ea7
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-07-09 23:51:55 +0200

    cpp: added .clangd file creation to post build

--- cpp/postBuild.sh
+++ cpp/postBuild.sh
@@ -2,3 +2,14 @@
 
 echo "Running post build script"
 wget -O cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
+
+
+INCLUDE_DIR="$(pwd)/inc"
+COMPILATION_DB="$(pwd)"
+
+cat > .clangd <<EOF
+CompileFlags:
+  Add:
+    - -I$INCLUDE_DIR
+  CompilationDatabase: "$COMPILATION_DB"
+EOF
