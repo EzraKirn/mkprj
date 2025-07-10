a5c05ac138064f88430d707696b2232eccb39406
EzraKirn <wyborskigrzesiek+web@gmail.com>
2025-07-10 21:49:02 +0200

    cpp: wget in postBuild made quiet

--- cpp/postBuild.sh
+++ cpp/postBuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running post build script"
-wget -O cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
+wget -q -O cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
 
 cat > .clangd <<EOF
 CompileFlags:
